a2a6ca447c6456b22b3387deda3b8d5ce3621f1b
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-08-12 20:12:01 +0530

    some changes

--- src/router/index.js
+++ src/router/index.js
@@ -30,9 +30,6 @@ const routes = [
   {
     path: '/pastevents',
     name: 'pastevents',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "pastevents" */ '../views/PastEventsView.vue'),
     meta: {
       title: "Past Events"

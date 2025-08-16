ad9edbffdaba230a065e8ecab04da908dbb97449
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-08-17 02:36:56 +0530

    link added in sidebar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -34,8 +34,8 @@
     </div>
 
     <div ref="sidebar" class="sidebar-menu" :class="{ 'is-open': clickMenu }">
-        <div class="sidebar-item">Home</div>
-        <div class="sidebar-item">About Us</div>
+        <a class="sidebar-item" href="/">Home</a>
+        <a class="sidebar-item" href="/about">About Us</a>
         <div class="sidebar-item event" @click="handleSidebarEvent">
             Events
             <img src="../assets/navbar/down-svg.svg" class="dropdown-icon" :class="{ 'event-click': clickSidebarEvent, 'event-reverse-click': !clickSidebarEvent && countSidebar > 0 }" alt="Dropdown Arrow"/>
@@ -44,18 +44,18 @@
         <!-- Sidebar Events Dropdown Menu -->
         <div class="sidebar-events-dropdown" :class="{ 'show': clickSidebarEvent }">
             <div class="dropdown-section">
-                <div class="dropdown-title">Pre-Events</div>
+                <a class="dropdown-title" href="/funfair">Pre-Events</a>
                 <div class="petal-shape petal-1"></div>
                 <div class="petal-shape petal-2"></div>
                 <div class="petal-shape petal-3"></div>
             </div>
             <div class="dropdown-divider"></div>
             <div class="dropdown-section">
-                <div class="dropdown-title">Past Editions</div>
+                <a class="dropdown-title" href="/our-journey">Past Editions</a>
                 <div class="petal-shape petal-4"></div>
             </div>
         </div>
-        <div class="sidebar-item">Speakers</div>
+        <a class="sidebar-item" href="/speakers">Speakers</a>
         <div class="sidebar-item sidebar-buy-tickets">Buy Tickets</div>
     </div>
 </template>
